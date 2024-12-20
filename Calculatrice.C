6d891528c19638aeea4ed283a46dca4fb17ce154
CMJ-crtl <149980271+creativmj@users.noreply.github.com>
2024-12-20 08:44:40 +0000

    Update Calculatrice.C

--- Calculatrice.C
+++ Calculatrice.C
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+//Declaration des variables
+
+//Fonction addition Moustapha
+
+//Fonction multiplication AMY
+
+//Fonction division Maurice
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
